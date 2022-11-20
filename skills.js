1843326b75ec143b9ebd94ffaf4887e0215737a8
cabralcaio <caio_cabral1@hotmail.com>
2022-11-20 18:20:24 -0300

    Lógica da div skills finalizada

--- skills.js
+++ skills.js
@@ -1,22 +1,38 @@
-const btn = document.getElementById("botao1")
+let itemShowed = document.getElementById("text")
+let boxSelected = document.querySelector(".skill-conteiner")
+console.log(boxSelected)
 
-btn.addEventListener("mouseenter", function (evento) {
-    evento.preventDefault()
-console.log("clicou")
-
-ocultar()
-
-function ocultar() {
-    document.getElementById("text1").style.display="none";
-}
-})
 
 const skill1 = document.querySelector(".skill1")
 skill1.addEventListener("mouseenter", function(evento) {
-    ocultar()
-
-function ocultar() {
-    document.getElementById("text1").style.display="none";
-}
+    ocultar(1)
 })
+
+const skill2 = document.querySelector(".skill2")
+skill2.addEventListener("mouseenter", function(evento) {
+    ocultar(2)
+})
+
+const skill3 = document.querySelector(".skill3")
+skill3.addEventListener("mouseenter", function(evento) {
+    ocultar(3)
+})
+
+const skill4 = document.querySelector(".skill4")
+skill4.addEventListener("mouseenter", function(evento) {
+    ocultar(4)
+})
+
+function ocultar(number) {
+    itemShowed.style.display="none"; //esconde o texto principal
+    document.getElementById(`text${number}`).style.display="list-item"; //mostra o texto do bloco que passou em cima
+    itemShowed = document.getElementById(`text${number}`)
+} 
+
+boxSelected.addEventListener("mouseleave", function(evento){
+    itemShowed.style.display="none"; //esconde o texto principal
+    itemShowed = document.getElementById("text")
+    document.getElementById("text").style.display="list-item"; //mostra o texto do bloco que passou em cima
+})
+
 
